dea4344b3fbf774db702808889660be7b844d2fc
Alexander Bright <131471830+ambright18@users.noreply.github.com>
2024-05-14 12:02:51 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Jokes from './components/Jokes/Jokes';
+function App() {
+  return (
+    <>
+    <h1>Joke Generator using a Joke API</h1> {/* API Courtesy of geeksforgeeks.com */}
+    <Jokes />
+    </>
+  );
+}
+
+export default App;
